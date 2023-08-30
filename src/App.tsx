e4156430b81c63aae610d491e25e14c14b4bfaf4
mousumi-ish <baral_mousumi@yahoo.com>
2023-08-30 13:39:22 +0100

    feat : checked JSK code

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,52 @@
-import './App.css';
-import Navbar from './components/navbar';
-import Header from './components/header';
-import Footer from './components/footer';
+import "./App.css";
+import Navbar from "./components/navbar";
+import Header from "./components/header";
+import Footer from "./components/footer";
 
 function App(): JSX.Element {
+  // JavaScript/TypeScript code can be inserted here!
 
-	// JavaScript/TypeScript code can be inserted here!
+  return (
+    <>
+      <Navbar />
+      <Header />
 
-	return (
-		<>
-			<Navbar />
-			<Header />
+      <main>
+        <div className="cards__wrapper">
+          {" "}
+          <div className="cards__wrapper">
+            <div className="card">
+              <h3 className="card__text card__header">Cindy Clawford</h3>
+              <img
+                className="card__image"
+                src="someImageUrlHere"
+                alt="A cute tabby kitten"
+              ></img>
+              <p className="card__text">Species: Cat</p>
+              <p className="card__text">Favourite Food(s): Mice</p>
+              <p className="card__text">Birth Year: 2012</p>
+            </div>
 
-			<main>
-				<div className='cards__wrapper'>{/* JSX code can go here */}</div>
-			</main>
+            <div className="card">
+              <h3 className="card__text card__header">Little Miss Purrfect</h3>
+              <img
+                className="card__image"
+                src="someImageUrlHere"
+                alt="A gray fluffy kitten"
+              ></img>
+              <p className="card__text">Species: Cat</p>
+              <p className="card__text">
+                Favourite Food(s): Wet food, dry food
+              </p>
+              <p className="card__text">Birth Year: 2016</p>
+            </div>
+          </div>
+        </div>
+      </main>
 
-			<Footer />
-		</>
-	);
+      <Footer />
+    </>
+  );
 }
 
 export default App;
