792ffabef0603555b567125fb22156c91d450138
mousumi-ish <baral_mousumi@yahoo.com>
2023-09-01 00:02:24 +0100

    feat: implement activity 8

--- src/components/cat_card.tsx
+++ src/components/cat_card.tsx
@@ -117,25 +117,31 @@ interface CatCardProps {
   birthYear: number;
   catIndex: number;
 }
-const CatCard: React.FC<CatCardProps> = (props) => {
+const CatCard: React.FC<CatCardProps> = ({
+  name,
+  species,
+  favFoods,
+  birthYear,
+  catIndex,
+}) => {
   //console.log("CatCard to App, come in App? Do you read me?", props);
-
+  // const { name, species, favFoods, birthYear, catIndex } = props;
   return (
     <div className="card">
-      {props.catIndex < images.length && (
+      {catIndex < images.length && (
         <CatImage
-          image={images[props.catIndex].image}
-          altText={images[props.catIndex].altText}
-          licenceType={images[props.catIndex].licenceType}
-          licenceUrl={images[props.catIndex].licenceUrl}
-          attributionName={images[props.catIndex].attributionName}
-          attributionUrl={images[props.catIndex].attributionUrl}
+          image={images[catIndex].image}
+          altText={images[catIndex].altText}
+          licenceType={images[catIndex].licenceType}
+          licenceUrl={images[catIndex].licenceUrl}
+          attributionName={images[catIndex].attributionName}
+          attributionUrl={images[catIndex].attributionUrl}
         />
       )}
-      <h3 className="card__text card__header">{props.name}</h3>
-      <p className="card__text">{props.species}</p>
-      <p className="card__text">{props.favFoods}</p>
-      <p className="card__text">{props.birthYear}</p>
+      <h3 className="card__text card__header">{name}</h3>
+      <p className="card__text">{species}</p>
+      <p className="card__text">{favFoods}</p>
+      <p className="card__text">{birthYear}</p>
     </div>
   );
 };
